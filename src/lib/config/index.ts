32e29abbdd798d8eb7b50d7b942f42eb71ed1b0f
Rigved Patki <rigved.patki@gmail.com>
2018-11-24 17:22:36 +0100

    added new dataset

--- src/lib/config/index.ts
+++ src/lib/config/index.ts
@@ -1,4 +1,9 @@
 export default {
   BUCKET: 'data-mining-assignment-3',
-  DATASETS_METADATA: ['amazon0312.txt', 'amazon0505.txt', 'amazon0601.txt']
+  DATASETS_METADATA: [
+    'email-Eu-core.txt',
+    'amazon0312.txt',
+    'amazon0505.txt',
+    'amazon0601.txt'
+  ]
 };
