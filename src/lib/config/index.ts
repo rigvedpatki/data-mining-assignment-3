a16fb7a660e406efd92c4bc0a33afb9dd0b80162
Rigved Patki <rigved.patki@gmail.com>
2018-11-24 19:46:30 +0100

    updated config

--- src/lib/config/index.ts
+++ src/lib/config/index.ts
@@ -1,15 +1,19 @@
 export default {
   BUCKET: 'data-mining-assignment-3',
   DATASETS_METADATA: [
-    'p2p-Gnutella04.txt',
-    'p2p-Gnutella05.txt',
-    'p2p-Gnutella06.txt',
-    'p2p-Gnutella08.txt',
-    'p2p-Gnutella09.txt',
-    'p2p-Gnutella24.txt',
-    'p2p-Gnutella25.txt',
-    'p2p-Gnutella30.txt',
-    'p2p-Gnutella31.txt'
+    // 'p2p-Gnutella04.txt',
+    // 'p2p-Gnutella05.txt',
+    // 'p2p-Gnutella06.txt',
+    // 'p2p-Gnutella08.txt',
+    // 'p2p-Gnutella09.txt',
+    // 'p2p-Gnutella24.txt',
+    // 'p2p-Gnutella25.txt',
+    // 'p2p-Gnutella30.txt',
+    // 'p2p-Gnutella31.txt'
+    'amazon0312.txt',
+    'amazon0505.txt',
+    'amazon0601.txt',
+    'email-Eu-core.txt'
   ],
-  LIMIT: 5000
+  LIMIT: 20000
 };
